7221ff2ff9a5da7b56079545db26210afbbd33c4
Benoît Legat <benoit.legat@gmail.com>
2025-04-08 16:12:30 +0200

    Add constraint conversion bridge (#17)
    * Add constraint conversion bridge
    
    * Add comment back
    
    * Fix format

--- src/Bridges/Constraint/Constraint.jl
+++ src/Bridges/Constraint/Constraint.jl
@@ -7,7 +7,18 @@ for filename in readdir(joinpath(@__DIR__, "bridges"); join = true)
     include(filename)
 end
 
+const ConversionBridge{W,T} = MOI.Bridges.Constraint.SetConversionBridge{
+    T,
+    LRO.LinearCombinationInSet{
+        W,
+        MOI.PositiveSemidefiniteConeTriangle,
+        LRO.TriangleVectorization{T,LRO.Factorization{T,Matrix{T},Vector{T}}},
+    },
+}
+
 function add_all_bridges(model, ::Type{T}) where {T}
+    MOI.Bridges.add_bridge(model, ConversionBridge{LRO.WITHOUT_SET,T})
+    MOI.Bridges.add_bridge(model, ConversionBridge{LRO.WITH_SET,T})
     MOI.Bridges.add_bridge(model, LinearCombinationBridge{T})
     MOI.Bridges.add_bridge(model, AppendZeroBridge{T})
     return
