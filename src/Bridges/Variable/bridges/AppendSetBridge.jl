f5411410f67a095f2716f57152df59f7ece7d583
Benoît Legat <benoit.legat@gmail.com>
2025-04-10 23:30:37 +0200

    Add LICENSE

--- src/Bridges/Variable/bridges/AppendSetBridge.jl
+++ src/Bridges/Variable/bridges/AppendSetBridge.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2024: Benoît Legat and contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 struct AppendSetBridge{T,S,V} <: MOI.Bridges.Variable.SetMapBridge{
     T,
     LRO.SetDotProducts{LRO.WITH_SET,S,V},
