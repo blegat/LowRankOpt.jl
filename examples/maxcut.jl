f8e8a8f0fd9173c987f05f8c8a581089966703c2
Benoît Legat <benoit.legat@gmail.com>
2025-04-16 15:58:57 +0200

    Fix format

--- examples/maxcut.jl
+++ examples/maxcut.jl
@@ -6,11 +6,19 @@ function maxcut(weights, solver)
     model = Model(solver)
     LRO.Bridges.add_all_bridges(backend(model).optimizer, Float64)
     cone = MOI.PositiveSemidefiniteConeTriangle(N)
-    factors = LRO.TriangleVectorization.(LRO.positive_semidefinite_factorization.(e_i.(1:N, N)))
+    factors = LRO.TriangleVectorization.(
+        LRO.positive_semidefinite_factorization.(e_i.(1:N, N)),
+    )
     set = LRO.SetDotProducts{LRO.WITH_SET}(cone, factors)
-    @variable(model, dot_prod_set[1:length(factors)+MOI.dimension(cone)] in set)
+    @variable(
+        model,
+        dot_prod_set[1:(length(factors)+MOI.dimension(cone))] in set
+    )
     dot_prod = dot_prod_set[1:length(factors)]
-    X = reshape_vector(dot_prod_set[length(factors).+(1:MOI.dimension(cone))], SymmetricMatrixShape(N))
+    X = reshape_vector(
+        dot_prod_set[length(factors) .+ (1:MOI.dimension(cone))],
+        SymmetricMatrixShape(N),
+    )
     @objective(model, Max, dot(L, X) / 4)
     @constraint(model, dot_prod .== 1)
     return model
