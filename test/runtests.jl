b6a16e50bce5cd75d0e4d7faff2ee72b3d07acd5
Benoît Legat <benoit.legat@gmail.com>
2025-07-29 11:18:10 +0200

    Fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,38 +3,17 @@
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
 
-module TestTest
-
 using Test
-import MathOptInterface as MOI
-import LowRankOpt as LRO
 
-function runtests()
-    for name in names(@__MODULE__; all = true)
-        if startswith("$name", "test_")
-            @testset "$(name)" begin
-                getfield(@__MODULE__, name)()
-            end
-        end
+files_to_exclude = ["runtests.jl"]
+for file in readdir(@__DIR__)
+    if isdir(joinpath(@__DIR__, file))
+        include(joinpath(@__DIR__, file, "runtests.jl"))
     end
-    return
+    if !endswith(file, ".jl") || any(isequal(file), files_to_exclude)
+        continue
+    end
+    @testset "$(file)" begin
+        include(joinpath(@__DIR__, file))
+    end
 end
-
-function test_runtests()
-    # Some tests are excluded because UniversalFallback accepts absolutely
-    # everything.
-    MOI.Test.runtests(
-        MOI.Utilities.MockOptimizer(
-            MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
-        ),
-        MOI.Test.Config(),
-        warn_unsupported = true,
-        verbose = true,
-        test_module = LRO.Test,
-    )
-    return
-end
-
-end  # module
-
-TestTest.runtests()
